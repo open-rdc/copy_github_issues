d5aa166339eaffa26754543002bedef18ba3b02f
mirai <yasuo.hayashibara@it-chiba.ac.jp>
2020-04-03 02:59:34 +0900

    typo error

--- copy_github_issues.rb
+++ copy_github_issues.rb
@@ -1,7 +1,7 @@
 require "octokit"
 
 source_repository      = "source_organization/source_repository"
-distination_repository = "distination_organization/distination_repository"
+destination_repository = "destination_organization/destination_repository"
 
 client = Octokit::Client.new login: "username", password: "password"
 client.auto_paginate = true
@@ -15,11 +15,11 @@ issues = issues_rev.reverse
 
 issues.each.with_index(1) do |issue, index|
   p "#{issue.number} #{issue.title}"
-  client.create_issue(distination_repository, issue.title, issue.body)
+  client.create_issue(destination_repository, issue.title, issue.body)
   comments = client.issue_comments source_repository, issue.number, options
   comments.each.with_index(1) do |comment, index|
     p "#{comment.body}"
-    client.add_comment(distination_repository, issue.number, comment.body)
+    client.add_comment(destination_repository, issue.number, comment.body)
     sleep(3)
   end
 end
