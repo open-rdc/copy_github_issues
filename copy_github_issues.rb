aa63f4538c513409330226be1a820230ba450c63
mirai <yasuo.hayashibara@it-chiba.ac.jp>
2020-04-03 02:27:30 +0900

    initial commit

---
+++ copy_github_issues.rb
@@ -0,0 +1,26 @@
+require "octokit"
+
+source_repository      = "source_organization/source_repository"
+distination_repository = "distination_organization/distination_repository"
+
+client = Octokit::Client.new login: "username", password: "password"
+client.auto_paginate = true
+
+options = {
+  state: :all,
+}
+
+issues_rev = client.list_issues source_repository, options
+issues = issues_rev.reverse
+
+issues.each.with_index(1) do |issue, index|
+  p "#{issue.number} #{issue.title}"
+  client.create_issue(distination_repository, issue.title, issue.body)
+  comments = client.issue_comments source_repository, issue.number, options
+  comments.each.with_index(1) do |comment, index|
+    p "#{comment.body}"
+    client.add_comment(distination_repository, issue.number, comment.body)
+    sleep(3)
+  end
+end
+
